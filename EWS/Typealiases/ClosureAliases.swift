7bf551aebd8a2964d3cd4b3d17a822b351504a46
Alvin <ling94alvin@gmail.com>
2019-04-09 15:20:47 -0500

    add place to define type aliases

---
+++ EWS/Typealiases/ClosureAliases.swift
@@ -0,0 +1,9 @@
+//
+//  ClosureAliases.swift
+//  EWS
+//
+//  Created by Alvin Ling on 4/9/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+typealias ErrorHandler = (Error?) -> Void
