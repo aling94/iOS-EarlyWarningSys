e0f5a4076dcee16db3e4be53e8a996b6032a941d
Alvin <ling94alvin@gmail.com>
2019-04-10 12:15:57 -0500

    break up core location functions and remove unused lifecycle methods

--- EWS/AppDelegate.swift
+++ EWS/AppDelegate.swift
@@ -15,48 +15,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var clManager: CLLocationManager!
-    var currentLocation: CLLocation!
+    var currentLocation: CLLocation?
     var locationName: String?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupCoreLocation()
+        requestLocation()
+        
         FirebaseApp.configure()
         FirebaseManager.shared.signoutUser()
-        
-        clManager = CLLocationManager()
-        clManager.delegate = self
-        clManager.requestWhenInUseAuthorization()
-        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
-            clManager.startUpdatingLocation()
-        }
-            
         return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 }
 
 extension AppDelegate: CLLocationManagerDelegate {
+    
+    var hasAllowedCoreLocation: Bool {
+        return CLLocationManager.authorizationStatus() == .authorizedWhenInUse
+    }
+    
+    func setupCoreLocation() {
+        clManager = CLLocationManager()
+        clManager.delegate = self
+    }
+    
+    func requestLocation() {
+        clManager.requestWhenInUseAuthorization()
+        if hasAllowedCoreLocation {
+            clManager.startUpdatingLocation()
+        }
+    }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
@@ -65,7 +53,7 @@ extension AppDelegate: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let loc = locations.first else { return }
+        guard let loc = manager.location else { return }
         print("-- Your Location: \(loc) --")
         currentLocation = loc
         manager.stopUpdatingLocation()
@@ -81,3 +69,4 @@ extension AppDelegate: CLLocationManagerDelegate {
     
 }
 
+let app = UIApplication.shared.delegate as! AppDelegate
