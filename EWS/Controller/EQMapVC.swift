d9ffd9953fcd4fc204fd03cf47a4b1fa63f24988
Alvin <ling94alvin@gmail.com>
2019-04-10 12:22:01 -0500

    add vc for displaying earthquakes on map

---
+++ EWS/Controller/EQMapVC.swift
@@ -0,0 +1,19 @@
+//
+//  EQMapVC.swift
+//  EWS
+//
+//  Created by Alvin Ling on 4/10/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+import UIKit
+
+class EQMapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
