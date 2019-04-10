55e6066f4de27e39aea5ee0877fbc38acaddc0c8
Alvin <ling94alvin@gmail.com>
2019-04-10 11:46:28 -0500

    add earthquake api

--- EWS/API/APIHandler.swift
+++ EWS/API/APIHandler.swift
@@ -11,19 +11,26 @@ import Alamofire
 import AlamofireObjectMapper
 
 let weatherURL = "https://api.darksky.net/forecast/1c32a0c10b1e247f0fc1f6ab8300b217/%d,%d"
+let earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson"
 
 class APIHandler : NSObject {
     override private init() {}
     
     static let shared = APIHandler()
     
-    func fetchWeatherData(_ lat : Double, _ long : Double, completion : @escaping (DarkSkyResponse?) -> Void ) {
+    func fetchWeatherData(_ lat : Double, _ long : Double, completion : @escaping (WeatherResponse?) -> Void ) {
         let requestURL = String(format: weatherURL, lat, long)
         
-        Alamofire.request(requestURL).responseObject { (response : DataResponse<DarkSkyResponse>) in
+        Alamofire.request(requestURL).responseObject { (response : DataResponse<WeatherResponse>) in
             let dsResponse = response.result.value
             completion(dsResponse)
         }
     }
     
+    func fetchEQData(completion : @escaping ([Earthquake]?) -> Void ) {
+        Alamofire.request(earthquakeURL).responseObject { (response : DataResponse<EarthquakeResponse>) in
+            let dsResponse = response.result.value
+            completion(dsResponse?.earthquakes)
+        }
+    }
 }
---
+++ EWS/Model/Earthquake.swift
@@ -0,0 +1,41 @@
+//
+//  Earthquake.swift
+//  EWS
+//
+//  Created by Alvin Ling on 4/10/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+import ObjectMapper
+
+class EarthquakeResponse: Mappable {
+    var earthquakes: [Earthquake]?
+    
+    required init?(map: Map) {}
+    
+    func mapping(map: Map) {
+        earthquakes <- map["features"]
+    }
+}
+
+class Earthquake: Mappable {
+    var id, status, place: String?
+    var mag: Double?
+    var time : UInt64?
+    var lat, long, depth: Double?
+    
+    required init?(map: Map) {}
+    
+    func mapping(map: Map) {
+        id <- map["id"]
+        
+        status <- map["properties.status"]
+        place <- map["properties.place"]
+        mag <- map["properties.mag"]
+        time <- map["properties.time"]
+        
+        lat <- map["geometry.coordinates.0"]
+        long <- map["geometry.coordinates.1"]
+        depth <- map["geometry.coordinates.2"]
+    }
+}
