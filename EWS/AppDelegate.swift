0a7fe90295e7322aa1d3bdf5f1a359b2652b39b3
Alvin <ling94alvin@gmail.com>
2019-04-11 10:19:12 -0500

    rename api keys

--- EWS/AppDelegate.swift
+++ EWS/AppDelegate.swift
@@ -22,8 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        GMSServices.provideAPIKey(GoogleAPIKey)
-        GMSPlacesClient.provideAPIKey(GoogleAPIKey)
+        GMSServices.provideAPIKey(GoogleAPIKeys.maps)
+        GMSPlacesClient.provideAPIKey(GoogleAPIKeys.places)
         setupCoreLocation()
         requestLocation()
         FirebaseApp.configure()
