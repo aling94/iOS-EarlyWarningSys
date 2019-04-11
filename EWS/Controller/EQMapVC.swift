bf71d946d2a2e805969a7924e130fc3bb872e6a4
Alvin <ling94alvin@gmail.com>
2019-04-10 22:16:08 -0500

    eq map needs a back button

--- EWS/Controller/EQMapVC.swift
+++ EWS/Controller/EQMapVC.swift
@@ -15,6 +15,7 @@ class EQMapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.setNavigationBarHidden(false, animated: false)
         setupGMsp()
         fetchEQData()
     }
