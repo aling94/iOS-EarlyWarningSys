db0429589f7a377dc90bc9e3471b7d51257c4cff
Alvin <ling94alvin@gmail.com>
2019-04-12 15:41:35 -0500

    post model class

---
+++ EWS/Model/Post.swift
@@ -0,0 +1,28 @@
+//
+//  Post.swift
+//  EWS
+//
+//  Created by Alvin Ling on 4/12/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+import UIKit
+
+final class PostInfo {
+    var description: String
+    var likeCount: Int
+    var pid: String
+    var uid: String
+    var timestamp: Double
+    var user: UserInfo?
+    var image: UIImage?
+    
+    
+    init(_ pid: String, info: [String : Any]) {
+        self.pid = pid
+        description = info["description"] as! String
+        likeCount = (info["likeCount"] as? Int) ?? 0
+        uid = info["uid"] as! String
+        timestamp = info["timestamp"] as! Double
+    }
+}
