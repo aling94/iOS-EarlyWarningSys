b5bc634022c75f1f58dd72bb2b53073824ac57fb
Alvin <ling94alvin@gmail.com>
2019-04-13 11:17:50 -0500

    add-post page added

--- EWS/Controller/PostsVC.swift
+++ EWS/Controller/PostsVC.swift
@@ -22,6 +22,7 @@ class PostsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Posts"
         makeNavBarClear()
         fetchPosts()
     }
