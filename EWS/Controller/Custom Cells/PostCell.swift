c5f310007416d7cfbdf2f7632a155b2a628cb2b9
Alvin <ling94alvin@gmail.com>
2019-04-13 00:26:15 -0500

    post table and cell design

---
+++ EWS/Controller/Custom Cells/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  EWS
+//
+//  Created by Alvin Ling on 4/13/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var userImage: CircleImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentText: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- EWS/Controller/PostsVC.swift
+++ EWS/Controller/PostsVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class PostsVC: UIViewController {
 
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
