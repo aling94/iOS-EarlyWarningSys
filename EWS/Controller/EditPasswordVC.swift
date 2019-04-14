dd6c6c065b6617f7aee6e20a61d88dd9d28b1e83
Alvin <ling94alvin@gmail.com>
2019-04-13 21:25:11 -0500

    edit pass page

--- EWS/Controller/EditPasswordVC.swift
+++ EWS/Controller/EditPasswordVC.swift
@@ -45,15 +45,25 @@ class EditPasswordVC: ProfileVC {
     }
     
     override func loadUserInfo() {
-        
+        let form = self.form
+        (form.rowBy(tag: "pass") as! PasswordFloatLabelRow).value = ""
+        (form.rowBy(tag: "cpass") as! PasswordFloatLabelRow).value = ""
+        DispatchQueue.main.async {
+            form.rows.forEach( {$0.reload()} )
+        }
     }
     
     @IBAction override func saveInfo(_ sender: Any) {
-        
+        let vals = form.values() as! [String : String]
+        if form.validate().isEmpty {
+            if vals["pass"] == vals["cpass"] {
+                let handler = alertError(successMsg: "Password changed.")
+                FirebaseManager.shared.updatePassword(vals["pass"]!, errorHandler: handler)
+            } else {
+                showAlert(title: "Oops", msg: "Passwords don't match!")
+            }
+        } else {
+            showAlert(title: "Oops", msg: "Invalid passwords.")
+        }
     }
-    
-    @IBAction override func resetFields(_ sender: Any) {
-        
-    }
-
 }
