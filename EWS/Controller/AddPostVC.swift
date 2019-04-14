a1d3466465cdfe85a2384fbc73163324791b1396
Alvin <ling94alvin@gmail.com>
2019-04-13 20:26:34 -0500

    add post functionality

--- EWS/Controller/AddPostVC.swift
+++ EWS/Controller/AddPostVC.swift
@@ -14,6 +14,8 @@ class AddPostVC: UIViewController {
     @IBOutlet weak var postImage: UIImageView!
     @IBOutlet weak var commentBox: UITextView!
     
+    var picChanged = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "New Post"
@@ -22,14 +24,64 @@ class AddPostVC: UIViewController {
         commentBox.placeholderColor = .white
     }
     
+    func changePostImage(_ image: UIImage?) {
+        guard let image = image else { return }
+        postImage.image = image
+        picChanged = true
+    }
+    
     @IBAction func savePost(_ sender: Any) {
+        
+        if let newPic = postImage.image, picChanged, !commentBox.text.isEmpty {
+            FirebaseManager.shared.addPost(img: newPic, postdesc: commentBox.text) { error in
+                if let error = error { self.showAlert(title: "Oops", msg: error.localizedDescription) }
+                else { self.navigationController?.popViewController(animated: true) }
+            }
+        } else {
+            var errors: [String] = []
+            
+            if commentBox.text.isEmpty {
+                errors.append("You've let the description box empty!")
+            }
+            if !picChanged {
+                errors.append("You haven't selected a photo to upload!")
+            }
+            showAlert(title: "Hey!", msg: errors.joined(separator: "\n\n"))
+        }
     }
     
     @IBAction func uploadImage(_ sender: Any) {
+        promptImageUpload()
     }
 }
 
 extension AddPostVC: UITextViewDelegate {
     
     
+}
+
+// MARK : - ImagePicker
+extension AddPostVC : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // Prompt the user to upload an image
+    func promptImageUpload() {
+        let imgPicker = UIImagePickerController()
+        let hasCamera = UIImagePickerController.isSourceTypeAvailable(.camera)
+        imgPicker.sourceType = hasCamera ? .camera : .photoLibrary
+        imgPicker.delegate = self
+        self.present(imgPicker, animated:true, completion:nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+        let selected = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        dismiss(animated: true) {
+            self.changePostImage(selected)
+        }
+    }
 }
