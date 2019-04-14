d8f4348320fd853d713cb08318a66a4199314958
Alvin <ling94alvin@gmail.com>
2019-04-13 20:57:48 -0500

    unused delegate

--- EWS/Controller/AddPostVC.swift
+++ EWS/Controller/AddPostVC.swift
@@ -55,11 +55,6 @@ class AddPostVC: UIViewController {
     }
 }
 
-extension AddPostVC: UITextViewDelegate {
-    
-    
-}
-
 // MARK : - ImagePicker
 extension AddPostVC : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
