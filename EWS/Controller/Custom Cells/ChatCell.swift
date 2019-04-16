130ac2f537895d5e2a5f13520d241f460822bca9
Alvin <ling94alvin@gmail.com>
2019-04-15 23:32:48 -0500

    chat table cell design

---
+++ EWS/Controller/Custom Cells/ChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatCell.swift
+//  EWS
+//
+//  Created by Alvin Ling on 4/15/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
