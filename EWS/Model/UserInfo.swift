21f768b6a495840750ba03defa205ae73f304774
Alvin <ling94alvin@gmail.com>
2019-04-19 01:18:32 -0500

    change properties to optionals for case with third party log ins

--- EWS/Model/UserInfo.swift
+++ EWS/Model/UserInfo.swift
@@ -11,57 +11,54 @@ import CoreLocation
 import FirebaseAuth
 
 final class UserInfo {
-    var fname, lname, email, dob, phone, gender : String
-    var latitude, longitude: Double
-    var location: String
     var uid: String
+    var fname, lname, email, dob, phone, gender : String?
+    var latitude, longitude: Double?
+    var location: String?
     var image: UIImage?
     
-    var name: String { return "\(fname) \(lname)"}
+    var name: String {
+        let result = "\(fname ?? "") \(lname ?? "")"
+        return result.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
     
-    var coords: CLLocationCoordinate2D {
-        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    var coords: CLLocationCoordinate2D? {
+        if let lat = latitude, let lon = longitude {
+            return CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        }
+        return nil
+        
     }
     
     init(_ uid: String) {
         self.uid = uid
-        email =  ""
-        phone = ""
-        fname =  ""
-        dob = ""
-        lname = ""
-        gender = "MALE"
-        location = ""
-        latitude = 0
-        longitude = 0
     }
     
     init(_ uid: String, info: [String: Any]) {
-        fname = info["fname"] as! String
-        lname = info["lname"] as! String
-        email = info["email"] as! String
-        dob = info["dob"] as! String
-        phone = info["phone"] as! String
-        gender = info["gender"] as! String
-        location = info["location"] as! String
-        latitude = info["latitude"] as! Double
-        longitude = info["longitude"] as! Double
         self.uid = uid
+        fname = info["fname"] as? String
+        lname = info["lname"] as? String
+        email = info["email"] as? String
+        dob = info["dob"] as? String
+        gender = info["gender"] as? String
+        phone = info["phone"] as? String
+        location = info["location"] as? String
+        latitude = info["latitude"] as? Double
+        longitude = info["longitude"] as? Double
     }
     
-    init(authInfo: AuthDataResult) {
-        let user = authInfo.user
-        email = user.email ?? ""
-        phone = user.phoneNumber ?? ""
-        uid = user.uid
-        fname = user.displayName ?? ""
-        dob = ""
-        lname = ""
-        gender = "MALE"
-        location = ""
-        latitude = 0
-        longitude = 0
+    func update(with info: [String : Any]) {
+        if let fname = info["fname"] as? String, !fname.isEmpty { self.fname = fname }
+        if let lname = info["lname"] as? String, !lname.isEmpty { self.lname = lname}
+        if let  email = info["email"] as? String, !email.isEmpty { self.email = email}
+        if let dob = info["dob"] as? String, !dob.isEmpty { self.dob = dob }
+        if let gender = info["gender"] as? String, !gender.isEmpty { self.gender = gender }
+        if let phone = info["phone"] as? String, !phone.isEmpty { self.phone = phone }
+        if let location = info["location"] as? String, !location.isEmpty { self.location = location }
+        if let latitude = info["latitude"] as? Double { self.latitude = latitude }
+        if let longitude = info["longitude"] as? Double { self.longitude = longitude }
     }
+    
     
     static func userDict(authInfo: AuthDataResult) -> [String : Any] {
         let user = authInfo.user
@@ -70,16 +67,10 @@ final class UserInfo {
             "email": user.email ?? "",
             "phone": user.phoneNumber ?? "",
             "fname": user.displayName ?? "",
-            "lname": "",
-            "dob": "",
-            "gender": "MALE",
-            "location": "",
-            "latitude": 0,
-            "longitude": 0
         ]
     }
     
     static func <(left: UserInfo, right: UserInfo) -> Bool {
-        return left.fname < right.fname
+        return (left.fname ?? "") < (right.fname ?? "")
     }
 }
